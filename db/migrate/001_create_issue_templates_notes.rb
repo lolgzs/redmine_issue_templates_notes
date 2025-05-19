958dd5572aef6dca86563a1f4c1c815b511dc27e
Laurent Laffont <llaffont@afi-sa.fr>
2025-05-19 17:44:22 +0200

    Redmine 6.0 fix

--- db/migrate/001_create_issue_templates_notes.rb
+++ db/migrate/001_create_issue_templates_notes.rb
@@ -1,6 +1,6 @@
 class CreateIssueTemplatesNotes < ActiveRecord::Migration[4.2]
   def change
-    create_table :issue_templates_notes do |t|
+    create_table :issue_templates_notes, if_not_exists: true do |t|
       t.column :template_name,     :string,     :null => false
       t.column :tracker_id,        :integer, :null => false
       t.column :author_id,         :integer, :null => false
@@ -9,8 +9,8 @@ class CreateIssueTemplatesNotes < ActiveRecord::Migration[4.2]
       t.column :created_on,        :timestamp
       t.column :updated_on,        :timestamp
     end
-    add_index :issue_templates_notes, :author_id
-    add_index :issue_templates_notes, :tracker_id
+    add_index :issue_templates_notes, :author_id, if_not_exists: true
+    add_index :issue_templates_notes, :tracker_id, if_not_exists: true
   end
   def self.down
     remove_index :issue_templates_notes, :author_id
